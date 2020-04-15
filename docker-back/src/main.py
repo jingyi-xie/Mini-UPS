093f0c65022232f20d81165f94da7fdea69431e9
fh65 <fh65@duke.edu>
2020-04-15 16:38:06 -0400

    add send/recv

--- docker-back/src/main.py
+++ docker-back/src/main.py
@@ -1,7 +1,8 @@
 import socket
 from proto import world_ups_pb2
 from command_helper import *
-
+from google.protobuf.internal.decoder import _DecodeVarint32
+from google.protobuf.internal.encoder import _EncodeVarint
 
 
 def main():
@@ -11,9 +12,19 @@ def main():
     world_s = socket.socket()
     world_s.connect((WORLD_HOST, WORLD_PORT))
     connectCommand = world_ups_pb2.UConnect()
-    createInitialConnect(connectCommand, TRUCK_NUM)
+    ENCODED_MESSAGE = createInitialConnect(connectCommand, TRUCK_NUM)
     
+    _EncodeVarint(world_s.send, len(ENCODED_MESSAGE), None)
 
+    var_int_buff = []
+    while True:
+        buf = world_socket.recv(1)
+        var_int_buff += buf
+        msg_len, new_pos = _DecodeVarint32(var_int_buff, 0)
+        if new_pos != 0:
+            break
+    whole_message = world_socket.recv(msg_len)
+    print(whole_message.SerializeToString())
 
 
 if __name__ == "__main__":
