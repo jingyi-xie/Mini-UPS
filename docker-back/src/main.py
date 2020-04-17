a86ea0a9cd7da75cff7892dbf185c4b62309af2b
fh65 <fh65@duke.edu>
2020-04-17 12:04:06 -0400

    send/recv wrapper

--- docker-back/src/main.py
+++ docker-back/src/main.py
@@ -4,6 +4,21 @@ from command_helper import *
 from google.protobuf.internal.decoder import _DecodeVarint32
 from google.protobuf.internal.encoder import _EncodeVarint
 
+def sender(socket, msg):
+    serialized = msg.SerializeToString()
+    _EncodeVarint(socket.send, len(serialized), None)
+    socket.send(serialized)
+
+def receiver(socket):
+    var_int_buff = []
+    while True:
+        buf = socket.recv(1)
+        var_int_buff += buf
+        msg_len, new_pos = _DecodeVarint32(var_int_buff, 0)
+        if new_pos != 0:
+            break
+    whole_message = socket.recv(msg_len)
+    return whole_message
 
 def main():
     WORLD_HOST = '127.0.0.1' 
@@ -13,17 +28,10 @@ def main():
     world_s.connect((WORLD_HOST, WORLD_PORT))
     connectCommand = world_ups_pb2.UConnect()
     ENCODED_MESSAGE = createInitialConnect(connectCommand, TRUCK_NUM)
-    
-    _EncodeVarint(world_s.send, len(ENCODED_MESSAGE.SerializeToString()), None)
-    world_s.send(ENCODED_MESSAGE.SerializeToString())
-    var_int_buff = []
-    while True:
-        buf = world_s.recv(1)
-        var_int_buff += buf
-        msg_len, new_pos = _DecodeVarint32(var_int_buff, 0)
-        if new_pos != 0:
-            break
-    whole_message = world_s.recv(msg_len)
+
+    sender(world_s, ENCODED_MESSAGE)
+    whole_message = receiver(world_s)
+
     connectResult = world_ups_pb2.UConnected()
     connectResult.ParseFromString(whole_message)
     print(connectResult)
