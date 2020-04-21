190d8d091303d367f02ce63a86a1005c09fea95a
fh65 <fh65@duke.edu>
2020-04-21 14:27:20 -0400

    add dict

--- docker-back/src/config.py
+++ docker-back/src/config.py
@@ -1,5 +1,10 @@
 def init():
-    global world_recv_acks
-    global amz_recv_acks
-    world_recv_acks = set()
-    amz_recv_acks = set()
+    global WORLD_RECV_SEQS
+    global AMZ_RECV_SEQS
+    WORLD_RECV_SEQS = set()
+    AMZ_RECV_SEQS = set()
+
+    global WORLD_MSG_MAP
+    global AMZ_MSG_MAP
+    WORLD_MSG_MAP = dict()
+    AMZ_MSG_MAP = dict()
