bfddba8e5244b484db6fef2e3886a1343fc59434
fh65 <fh65@duke.edu>
2020-04-21 16:03:57 -0400

    resend message

--- docker-back/src/amazon_helper.py
+++ docker-back/src/amazon_helper.py
@@ -3,6 +3,7 @@ from socket_helper import *
 from command_helper import *
 from world_helper import *
 from db_update import *
+import time
 
 def sendWorldID(amz_socket, worldID, seqNum):
     #Create the Umsg that contains worldid
@@ -31,8 +32,10 @@ def findPkgX(csr, pkgid):
 
 def processAmsg(con, msg, ASEQ, WSEQ):
     csr = con.cursor()
-    world_msg = world_ups_pb2.UCommands()
-    amazon_msg = IG1_pb2.UMsg()
+
+    # lists of messages to send
+    world_list = {}
+    amz_list = []
 
     for item in msg.asendtruck:
         # 1. update truck
@@ -52,14 +55,18 @@ def processAmsg(con, msg, ASEQ, WSEQ):
                          status, product_name, truck_id)
         
         # 3. world go pick up
+        world_msg = world_ups_pb2.UCommands() # world msg 1
         pickup = world_msg.pickups.add()
         pickup.truckid = truck_id
         pickup.whid = item.whinfo.whid
         WSEQ += 1
         pickup.seqnum = WSEQ
+        # world_list.append(world_msg)
+        world_list[WSEQ] = world_msg # add to map
 
         # 4. reply amazon
         # orderplaced
+        amazon_msg = IG1_pb2.UMsg() # amz msg1
         placed = amazon_msg.uorderplaced.add()
         placed.pkgid = package_id
         placed.truckid = truck_id
@@ -67,6 +74,7 @@ def processAmsg(con, msg, ASEQ, WSEQ):
         placed.seq = ASEQ
         # ack
         amazon_msg.ack.append(item.seq)
+        amz_list.append(amazon_msg)
         
 
     for item in msg.afinishloading:
@@ -75,6 +83,7 @@ def processAmsg(con, msg, ASEQ, WSEQ):
         db_updatePackage(csr, item.pkgid, 'shipping')
 
         # 2. world go deliver
+        world_msg = world_ups_pb2.UCommands()  # world msg 2
         deliver = world_msg.deliveries.add()
         deliver.truckid = item.truckid
         location = deliver.packages.add()
@@ -84,18 +93,17 @@ def processAmsg(con, msg, ASEQ, WSEQ):
         location.y = xy[1]
         WSEQ += 1
         deliver.seqnum = WSEQ
+        # world_list.append(world_msg)
+        world_list[WSEQ] = world_msg  # add to map
 
         # 3. reply amazon
+        amazon_msg = IG1_pb2.UMsg()  # amz msg2
         amazon_msg.ack.append(item.seq)
-
-    #  # TODO
-    # for item in msg.ack:
-    #     # 1. compare with seq
-    #     # 2. resend message
+        amz_list.append(amazon_msg)
 
     csr.close()
     con.commit()
-    return amazon_msg, world_msg
+    return amz_list, world_list
 
 
 def getProductName(products):
@@ -106,10 +114,22 @@ def getProductName(products):
 
 
 def process_aTask(con, msg, wSocket, aSocket, ASEQ, WSEQ):
-    amazon_msg, world_msg = processAmsg(con, msg, ASEQ, WSEQ)
-    # send message to world and amazon
-    sender(wSocket, world_msg)
-    sender(aSocket, amazon_msg)
+    global WORLD_RECV_ACKS
+    amz_list, world_list = processAmsg(con, msg, ASEQ, WSEQ)
+
+    # send message to amazon
+    for item in amz_list:
+        sender(wSocket, item)
+    
+    # repeatedly messages to world
+    while len(world_list):
+        for key in world_list:
+            if key in WORLD_RECV_ACKS:
+                del world_list[key]
+            else:
+                sender(wSocket, world_list[key])
+        time.sleep(5)
+    
 
 # # TEST ========== processAmsg
 # msg = IG1_pb2.AMsg()
@@ -140,8 +160,10 @@ def process_aTask(con, msg, wSocket, aSocket, ASEQ, WSEQ):
 # amsg, wmsg = processAmsg(con, msg, 0, 0)
 
 # disconnectDB(con)
-# print("Amazon message ========== \n" + str(amsg))
-# print("World message ========== \n" + str(wmsg))
+# print("Amazon message ==========")
+# print(amsg)
+# print("World message ==========")
+# print(wmsg)
 
 
 # # TEST ========== findIdleTruck
--- docker-back/src/config.py
+++ docker-back/src/config.py
@@ -1,10 +1,9 @@
 def init():
     global WORLD_RECV_SEQS
-    global AMZ_RECV_SEQS
     WORLD_RECV_SEQS = set()
-    AMZ_RECV_SEQS = set()
+
+    global WORLD_RECV_ACKS
+    WORLD_RECV_ACKS = set()
 
     global WORLD_MSG_MAP
-    global AMZ_MSG_MAP
     WORLD_MSG_MAP = dict()
-    AMZ_MSG_MAP = dict()
