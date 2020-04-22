b03def19d203c93bf5f8ea217220253505518ed1
jx95 <jx95@duke.edu>
2020-04-22 11:46:05 -0400

    fix email

--- docker-back/src/email_helper.py
+++ docker-back/src/email_helper.py
@@ -3,7 +3,7 @@ import yagmail
 receiver = "hf65@duke.edu"
 body = "Hello there from Yagmail"
 
-yag = yagmail.SMTP("ece568ups@gmail.com")
+yag = yagmail.SMTP('ece568ups@gmail.com', '568upsece')
 yag.send(
     to=receiver,
     subject="Yagmail test with attachment",
